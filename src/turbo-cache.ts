c48f560d2ffe8104057706a18374085e1d4f9c5d
JJ Kasper <jj@jjsweb.site>
2023-08-25 15:46:12 -0700

    Polyfill fetch and add team slug

--- src/turbo-cache.ts
+++ src/turbo-cache.ts
@@ -2,14 +2,16 @@ import os from "os";
 import tar from "tar";
 import path from "path";
 import fs from "fs-extra";
+import fetch from "node-fetch";
 
-export {ReserveCacheError, ValidationError} from '@actions/cache'
+export { ReserveCacheError, ValidationError } from "@actions/cache";
 
 type CacheInt = typeof import("@actions/cache");
 
 export const isFeatureAvailable: CacheInt["isFeatureAvailable"] = () => true;
 
 const turboApi = process.env.TURBO_API || "https://vercel.com";
+const turboTeam = process.env.TURBO_TEAM;
 const turboToken = process.env.TURBO_TOKEN;
 
 if (!turboToken) {
@@ -50,13 +52,16 @@ export const saveCache: CacheInt["saveCache"] = async function saveCache(
 
   const body = await fs.readFile(`${workDir}.tgz`);
 
-  const res = await fetch(`${turboApi}/v8/artifacts/${key}`, {
-    method: "PUT",
-    headers: {
-      Authorization: `Bearer ${turboToken}`,
-    },
-    body,
-  });
+  const res = await fetch(
+    `${turboApi}/v8/artifacts/${key}${turboTeam ? `?slug=${turboTeam}` : ""}`,
+    {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${turboToken}`,
+      },
+      body,
+    }
+  );
 
   if (!res.ok) {
     console.error(`Failed to save cache ${res.status}, ${await res.text()}`);
@@ -72,12 +77,17 @@ export const restoreCache: CacheInt["restoreCache"] =
     let restoreKey = "";
 
     for (const key of keys) {
-      const res = await fetch(`${turboApi}/v8/artifacts/${key}`, {
-        method: "HEAD",
-        headers: {
-          Authorization: `Bearer ${turboToken}`,
-        },
-      });
+      const res = await fetch(
+        `${turboApi}/v8/artifacts/${key}${
+          turboTeam ? `?slug=${turboTeam}` : ""
+        }`,
+        {
+          method: "HEAD",
+          headers: {
+            Authorization: `Bearer ${turboToken}`,
+          },
+        }
+      );
 
       if (res.ok) {
         restoreKey = key;
@@ -93,31 +103,38 @@ export const restoreCache: CacheInt["restoreCache"] =
 
     const cacheFile = path.join(os.tmpdir(), `rust-cache-${restoreKey}.tgz`);
 
-    const res = await fetch(`${turboApi}/v8/artifacts/${restoreKey}`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${turboToken}`,
-      },
-    });
+    const res = await fetch(
+      `${turboApi}/v8/artifacts/${restoreKey}${
+        turboTeam ? `?slug=${turboTeam}` : ""
+      }`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${turboToken}`,
+        },
+      }
+    );
 
     if (!res.ok) {
-      return
+      return;
     }
 
     await fs.writeFile(cacheFile, Buffer.from(await res.arrayBuffer()));
     console.log("Wrote cache file", cacheFile);
 
     const workDir = path.join(os.tmpdir(), `rust-cache-${Date.now()}`);
-    await fs.mkdir(workDir, { recursive: true })
-    
+    await fs.mkdir(workDir, { recursive: true });
+
     await tar.x({
       cwd: workDir,
       file: cacheFile,
     });
-    
+
     for (const p of await fs.readdir(workDir)) {
-      console.log('moving', p, 'from cache');
-      await fs.move(path.join(workDir, p), path.join(process.cwd(), p), { overwrite: true })
+      console.log("moving", p, "from cache");
+      await fs.move(path.join(workDir, p), path.join(process.cwd(), p), {
+        overwrite: true,
+      });
     }
     return restoreKey;
   };
